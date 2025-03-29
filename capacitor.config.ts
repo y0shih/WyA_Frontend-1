eccfddfa3e7d1634fe93e4158ac07fa3537728f6
duy08k4 <duytran.290804@gmail.com>
2025-03-29 09:01:54 +0700

    Based Project (Not using for coding)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.wyafrontend',
+  appName: 'wyafrontend',
+  webDir: 'dist'
+};
+
+export default config;
